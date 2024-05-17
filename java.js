a995490f41526f439aba36d3df6662a7e5c63935
PI123S <petar.d.antonov.2023@elsys-bg.org>
2024-05-17 23:51:25 +0300

    Added a js function

--- java.js
+++ java.js
@@ -22,3 +22,13 @@ document.addEventListener('DOMContentLoaded', function() {
     }
  }
 
+ let popup = document.getElementById('popup')
+
+function openPopup(){
+  popup.classList.add('open-popup')
+}
+
+function closePopup(){
+  popup.classList.remove('open-popup')
+}
+
